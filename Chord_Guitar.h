5fb1520050457f75347f0c7eae19c79ec3b63d74
Anh-Khoa <anhkhoa.than@colorado.edu>
2015-04-20 21:31:56 -0600

    this is the header file

---
+++ Chord_Guitar.h
@@ -0,0 +1,57 @@
+//Chord_Guitar.h
+#include <iostream>
+#include <string>
+#ifndef GUITAR_H
+#define GUITAR_H
+//Anh-Khoa Than
+//Thomas
+//Yash
+struct node
+{
+	std::string chord;
+    std::string string_guitar1;
+    std::string string_guitar2;
+    std::string string_guitar3;
+    std::string string_guitar4;
+    std::string string_guitar5;
+    std::string string_guitar6;
+    node(){};
+    node *next;
+    node *previous;
+
+};
+
+class Guitar
+{
+    public:
+        Guitar();
+        //virtual~Guitar();
+        void chord_creation(std::string chord, std::string string_guitar1, std::string string_guitar2, std::string string_guitar3, std::string string_guitar4, std::string string_guitar5, std::string string_guitar6);
+        void find_chord(std::string chord);
+        void add_chord(std::string chord, std::string string_guitar1, std::string string_guitar2, std::string string_guitar3, std::string string_guitar4, std::string string_guitar5, std::string string_guitar6);
+        void delete_chord(std::string chord);
+        void transpose_chord_up(std::string chord);
+        void transpose_chord_down(std::string chord);
+        void change_chord(std::string chord, std::string string_guitar1, std::string string_guitar2, std::string string_guitar3, std::string string_guitar4, std::string string_guitar5, std::string string_guitar6);
+        void display_chord();
+        void write_to_file();
+    protected:
+    private:
+    node *head;
+    node *tail;
+    bool start_creation = false;
+};
+
+#endif // GUITAR_H
+
+//List of Public Methods
+//1. create a chord list with a file
+//2. display a chord
+//3. add a chord
+//4. delete a chord
+//5. transpose up a selected chord
+//6. transpose down a selected chord
+//7. show a current list of chords
+//8. clear all chords
+//9. change a chord
+//10. save changes
